af87a3dd43634bbeac5d7abd177abb3af3954b00
Yunus Yarba <yunusas7@gmail.com>
2019-02-10 23:46:07 +0300

    config.php file edited.

--- app/src/main/java/com/example/dell/proje/Config.java
+++ app/src/main/java/com/example/dell/proje/Config.java
@@ -2,5 +2,5 @@ package com.example.dell.proje;
 
 public class Config {
 
-   public  final static String  URL = "http://192.168.1.4/wbservis/service/";
+   public  final static String  URL = "http://192.168.1.11/wbservis/service/";
 }
